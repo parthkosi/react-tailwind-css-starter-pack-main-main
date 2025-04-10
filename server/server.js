ceabc394a65ee5f64a86a93b740946599bad7d23
Parth Agrawal <parthkosi47@gmail.com>
2025-04-10 17:20:02 +0530

    .

--- server/server.js
+++ server/server.js
@@ -12,6 +12,11 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 
 // Middleware
