cb5454818c21ef5587d187364caf1a9e42e898ea
Parth Agrawal <parthkosi47@gmail.com>
2025-05-14 17:12:51 +0530

    .

--- server/config/database.js
+++ server/config/database.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 const dbconnect = ()=> {
     mongoose.connect(process.env.MONGO_URL,{
-        // useNewUrlParser : true,
-        // useUnifiedTopology : true,
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
     })
     .then(() =>console.log("DB ka connection successfully"))
     .catch((error)=>{
